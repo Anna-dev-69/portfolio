bd082b4aff52cd43b52241fdfa72dd9c3abbe526
Anna Grishnenko <anya.grishnenko@gmail.com>
2025-04-01 16:14:16 +0300

    fix experience

--- src/data/experience-list.ts
+++ src/data/experience-list.ts
@@ -1,7 +1,9 @@
 import myProject from "../assets/myProject1.png";
 import myProject2 from "../assets/myProject2.png";
-import image from "../assets/image.png";
 import loft from "../assets/loft.png";
+import infinite from "../assets/infinite.png";
+import alfa from "../assets/alfa.png";
+import moon from "../assets/moon.png";
 
 const experienceList = [
   {
@@ -16,8 +18,23 @@ const experienceList = [
   },
   {
     id: "3",
+    href: " https://anna-dev-69.github.io/ecosystem-alfa/#/",
+    imgSrc: alfa,
+  },
+  {
+    id: "4",
+    href: "https://anna-dev-69.github.io/infinite/",
+    imgSrc: infinite,
+  },
+  {
+    id: "5",
     href: "https://loft-ts-wc9a.vercel.app/",
     imgSrc: loft,
+  },
+  {
+    id: "6",
+    href: "https://moon-ashen.vercel.app/",
+    imgSrc: moon,
   },
 ];
 export default experienceList;
